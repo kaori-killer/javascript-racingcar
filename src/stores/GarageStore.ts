87d0827b423dce8b9974b4139284119df4490a49
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2025-04-21 14:19:25 +0900

    refactor: Store 추상 메서드를 인터페이스로 변경

--- src/stores/GarageStore.ts
+++ src/stores/GarageStore.ts
@@ -3,25 +3,24 @@ import Store from "./Store.ts";
 import Garage from "../domain/Garage.ts";
 import Car from "../domain/Car.ts";
 
+// 💡 독립적인 인스턴스 생성하는 구문
+// super();
+
 export type GarageStoreSnapshot = {
   cars: Car[];
 };
 
 // ❓ 추상 클래스 상속과 인터페이스를 둘다 받는 건 불가능할까? 둘다 할 필요가 없을까?
-export default class GarageStore extends Store<GarageStoreSnapshot> {
+export default class GarageStore implements Store<GarageStoreSnapshot> {
   private garage: Garage;
 
   // 📚 필요한 의존성을 객체가 직접 생성하는 대신 우리가 ctor를 통해 주입한다.
   constructor(garage: Garage) {
-    // 💡 독립적인 인스턴스 생성하는 구문
-    super();
     this.garage = garage;
-    this.takeSnapshot();
   }
 
   init() {
     this.garage = this.garage.init();
-    this.takeSnapshot();
   }
 
   addCars({ carNames, position }: { carNames: string[]; position: number }) {
@@ -30,15 +29,14 @@ export default class GarageStore extends Store<GarageStoreSnapshot> {
 
   addCar({ name, position }: { name: string; position: number }) {
     this.garage = this.garage.withCar({ name, position });
-    this.takeSnapshot();
   }
 
   winnerNames(): string[] {
     return this.garage.winnerNames();
   }
 
-  protected takeSnapshot() {
-    this.snapshot = {
+  get snapshot(): GarageStoreSnapshot {
+    return {
       cars: this.garage.cars,
     };
   }
--- src/stores/Store.ts
+++ src/stores/Store.ts
@@ -1,11 +1,8 @@
 // 💡 abstract 클래스: 인스턴스 생성 불가
-export default abstract class Store<SnapShot> {
-  snapshot = {} as SnapShot;
+// abstract 메서드: 자식 클래스에서 반드시 구현해야 함
 
-  getSnapshot(): SnapShot {
-    return this.snapshot;
-  }
-
-  // abstract 메서드: 자식 클래스에서 반드시 구현해야 함
-  protected abstract takeSnapshot(): void;
+interface Store<SnapShot> {
+  readonly snapshot: SnapShot;
 }
+
+export default Store;
